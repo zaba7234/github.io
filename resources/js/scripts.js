c375fdd6085b952e3023e1a99fe1e1b6cc2775a9
Zachary Baker <zaba7234@colorado.edu>
2022-04-26 14:52:09 -0600

    Doing some ejs experiments

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,19 +1,28 @@
 const hamburger = document.querySelector("#hamburger");
+const navbar = document.querySelector(".navbar-nav");
 
 hamburger.addEventListener("click", async function(event) {
     // console.log("HAMBURGER!!!");
-    const navbar = document.querySelector(".navbar-nav");
     navbar.classList.toggle("mobile");
     navbar.classList.toggle("slide");
 });
 
 const hideLink = document.querySelector("#navbarDropdown");
+const hidden1 = document.querySelector("#hideLink");
+const hidden2 = document.querySelector("#hideLink2");
 
 hideLink.addEventListener("click", async function(event) {
     // console.log("HIDE!!!");
-    const hidden1 = document.querySelector("#hideLink");
-    const hidden2 = document.querySelector("#hideLink2");
     // console.log(hidden1, hidden2);
     hidden1.classList.toggle("hidden");
     hidden2.classList.toggle("hidden");
 }); 
+
+document.addEventListener("click", async function(event) {
+    // console.log("clicked")
+    if (hidden1.classList.contains("hidden") && navbar.classList.contains("mobile"))
+    {
+        hidden1.classList.toggle("hidden");
+        hidden2.classList.toggle("hidden");
+    }
+});
